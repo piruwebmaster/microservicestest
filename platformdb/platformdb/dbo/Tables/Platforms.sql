d7e674641554c97d70478986b3e339eefd9ed148
Byron Ramos <byronjesus1993@gmail.com>
2022-12-31 22:34:21 -0600

    .

---
+++ platformdb/platformdb/dbo/Tables/Platforms.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Platforms] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (MAX) NOT NULL,
+    [Publisher] NVARCHAR (MAX) NOT NULL,
+    [Cost]      NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Platforms] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
